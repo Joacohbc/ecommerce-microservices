c0458d54698bc76694f12b6c7c6872b65c0f3177
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-04-10 23:26:36 -0300

    Add DuplicatedRelationException

---
+++ products-service/src/main/java/com/microecommerce/productsservice/exceptions/DuplicatedRelationException.java
@@ -0,0 +1,7 @@
+package com.microecommerce.productsservice.exceptions;
+
+public class DuplicatedRelationException extends Exception {
+    public DuplicatedRelationException(String message) {
+        super(message);
+    }
+}
