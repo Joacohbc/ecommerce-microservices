a5b9a5e935adf9dc7706134f165cfcaf811142bf
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-04-12 10:36:49 -0300

    Add explanation to EntityNotFoundException and RelatedEntityNotFoundException

--- products-service/src/main/java/com/microecommerce/productsservice/exceptions/EntityNotFoundException.java
+++ products-service/src/main/java/com/microecommerce/productsservice/exceptions/EntityNotFoundException.java
@@ -1,5 +1,6 @@
 package com.microecommerce.productsservice.exceptions;
 
+// Use: When the main entity in the operation is not found
 public class EntityNotFoundException extends Exception {
     public EntityNotFoundException(String message) {
         super(message);
