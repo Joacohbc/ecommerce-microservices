6b8ff6980e4ab0f5c42170ee569471b0bbd9cd74
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-04-20 18:28:55 -0300

    (orders-service): Fix createdAt and updatedAt in order

--- orders-service/src/main/java/com/microecommerce/ordersservice/models/Order.java
+++ orders-service/src/main/java/com/microecommerce/ordersservice/models/Order.java
@@ -1,34 +1,55 @@
 package com.microecommerce.ordersservice.models;
 
 import jakarta.persistence.Id;
+import jakarta.persistence.PreUpdate;
 import lombok.*;
+import org.bson.types.ObjectId;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @Document("orders")
 @Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Order implements Serializable {
 
     @Id
-    private Long id;
+    private String id;
+
+    @Setter
     private Long customerId;
+
+    @Setter
     private List<Item> items;
+
+    @Setter
     private OrderStatus status;
+
+    @Setter
     private Double total;
+
+    @Setter
     private LocalDateTime finishedAt;
 
-    @CreationTimestamp
-    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    @UpdateTimestamp
-    private String updatedAt;
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return new ObjectId(id).
+                getDate()
+                .toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
 }
