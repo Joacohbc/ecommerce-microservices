a3fe352eea50a52ff336e1cf6509743adc3f824d
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-04-27 11:47:10 -0300

    (orders-service, product-service): Implement OrderRestExceptionHandler and ProductRestExceptionHandler

---
+++ products-service/src/main/java/com/microecommerce/productsservice/controllers/ProductRestExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.microecommerce.productsservice.controllers;
+
+import com.microecommerce.utilitymodule.exceptions.RestExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ProductRestExceptionHandler extends RestExceptionHandler {
+}
