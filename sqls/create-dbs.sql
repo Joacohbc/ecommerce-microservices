5c81cbf9f91c35630622bbd44c876cee0d548b6b
Joacohbc <Joacohbc@test.com>
2024-10-21 02:28:58 +0000

    (fileservice): Move it to MySQL

--- sqls/create-dbs.sql
+++ sqls/create-dbs.sql
@@ -1,3 +1,4 @@
+-- Active: 1724543067802@@mysql@3306@devdb
 CREATE DATABASE ecommerce_products
     DEFAULT CHARACTER SET = 'utf8mb4';
 
@@ -17,3 +18,8 @@ CREATE DATABASE ecommerce_users_auth
     DEFAULT CHARACTER SET = 'utf8mb4';
 
 GRANT ALL PRIVILEGES ON ecommerce_users_auth.* TO 'devuser'@'%';
+
+CREATE DATABASE ecommerce_filestorage
+    DEFAULT CHARACTER SET = 'utf8mb4';
+
+GRANT ALL PRIVILEGES ON ecommerce_filestorage.* TO 'devuser'@'%';
