375d1c2590e1bb9f43178a82f8e820158df021cc
Joacohbc <joaquingenovag8@gmail.com>
2025-02-08 16:51:31 -0300

    (utility-module):Enhance exception handling

--- utility-module/src/main/java/com/microecommerce/utilitymodule/exceptions/DuplicatedRelationException.java
+++ utility-module/src/main/java/com/microecommerce/utilitymodule/exceptions/DuplicatedRelationException.java
@@ -1,5 +1,6 @@
 package com.microecommerce.utilitymodule.exceptions;
 
+// Use: When a relation is duplicated (e.g. a user is already a member of a group)
 public class DuplicatedRelationException extends Exception {
     public DuplicatedRelationException(String message) {
         super(message);
