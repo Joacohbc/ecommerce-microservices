337be3e30211dd9c5d6838baff1694a979c8b1b0
Joacohbc <joaquingenovag8@gmail.com>
2025-02-08 16:51:57 -0300

    (file-service): Implement enhance file handling methods

---
+++ fileservice/src/main/java/com/microecommerce/fileservice/controllers/FileRestExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.microecommerce.fileservice.controllers;
+
+import com.microecommerce.utilitymodule.exceptions.RestExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class FileRestExceptionHandler extends RestExceptionHandler {
+}
