68be5682c0fc2a6638635d4db4f61447e18957ea
Joacohbc <joaquingenovag8@gmail.com>
2025-02-08 19:55:12 -0300

    (file-storage): exclude Mongo auto-configuration

--- fileservice/src/main/java/com/microecommerce/fileservice/FileServiceApplication.java
+++ fileservice/src/main/java/com/microecommerce/fileservice/FileServiceApplication.java
@@ -2,8 +2,10 @@ package com.microecommerce.fileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class FileServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FileServiceApplication.class, args);
